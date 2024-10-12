efd8551c863a9b1dc3d20ffa1ad5b10a33d205a3
2Day2Early <sche0302@student.monash.edu>
2024-10-12 20:50:09 +0800

    Format the code

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,36 +1,36 @@
 var radar = "js/radar_overall.vg.json";
-vegaEmbed('#radar-chart', radar, { "actions": false }).then(result => {}).catch(console.error);
+vegaEmbed('#radar-chart', radar, { "actions": false }).then(result => { }).catch(console.error);
 
 var map = "js/map_establishments.vg.json";
-vegaEmbed('#map', map, { "actions": false }).then(result => {}).catch(console.error);
+vegaEmbed('#map', map, { "actions": false }).then(result => { }).catch(console.error);
 
 var stacked = "js/concat.vg.json";
-vegaEmbed('#stacked-chart', stacked, { "actions": false }).then(result => {}).catch(console.error);
+vegaEmbed('#stacked-chart', stacked, { "actions": false }).then(result => { }).catch(console.error);
 
 var bump = "js/bump.vg.json";
-vegaEmbed('#bump-chart', bump, { "actions": false }).then(result => {}).catch(console.error);
+vegaEmbed('#bump-chart', bump, { "actions": false }).then(result => { }).catch(console.error);
 
 var heatMap = "js/heatMap.vg.json";
-vegaEmbed('#heatmap', heatMap, { "actions": false }).then(result => {}).catch(console.error);
+vegaEmbed('#heatmap', heatMap, { "actions": false }).then(result => { }).catch(console.error);
 
 // Function to switch radar chart
 const switchRadar = (radar) => {
-    vegaEmbed('#radar-chart', radar, {"actions": false}).then(result => {}).catch(console.error);
+    vegaEmbed('#radar-chart', radar, { "actions": false }).then(result => { }).catch(console.error);
 }
 
 // Function to switch map
 const switchMap = (map) => {
-    vegaEmbed('#map', map, {"actions": false}).then(result => {}).catch(console.error);
+    vegaEmbed('#map', map, { "actions": false }).then(result => { }).catch(console.error);
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
-    document.getElementById("radar-select").addEventListener("change", function() {
+    document.getElementById("radar-select").addEventListener("change", function () {
         const value = document.getElementById("radar-select").value;
         switchRadar(value);
     });
 
-    document.getElementById("map-select").addEventListener("change", function() {
+    document.getElementById("map-select").addEventListener("change", function () {
         const value = document.getElementById("map-select").value;
         switchMap(value);
     });
